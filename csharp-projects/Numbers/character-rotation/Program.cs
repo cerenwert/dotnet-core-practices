8724e699eda1a3543fdb6bab750e618216bc1cd5
ceren <79611941+cerenwert@users.noreply.github.com>
2026-03-14 17:32:30 +0300

    Implement character rotation for input words
    Refactor character rotation logic to process input words and rotate characters.

--- csharp-projects/Numbers/character-rotation/Program.cs
+++ csharp-projects/Numbers/character-rotation/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+        string [] word= input.Split(' ');
+        foreach (char word in words[0])
+        {
+           Console.WriteLine(
+            word.Length>1 
+            ? word.Substring(1) + word[0] : word +" "
+           );
+        }
+    }
+}

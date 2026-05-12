758e4e5d66919080c88df78d380fd811c5610eb0
Ceren Akçay <ceren1akcay@gmail.com>
2026-05-13 01:51:21 +0300

    Root dosyaları temizlendi

--- csharp-projects/Numbers/index-remover/Program.cs
+++ csharp-projects/Numbers/index-remover/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO.Pipelines;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Input: ");
+        Console.Write("yazınız: ");
         string input = Console.ReadLine();
         string result = RemoveIndexes(input);
         Console.WriteLine("Output: " + result);
 
+    }
+    static string RemoveIndexes(string input)
+    {
+        string[] parts = input.Split(' ');
+        string word = parts[0];
+        int index = int.Parse(parts[1]);
+        if (index < 0 || index >= word.Length)
+        {
+            Console.WriteLine("Geçersiz indeks!");
+            return word;
+        }
+        return word.Remove(index, 1);
     }
 }

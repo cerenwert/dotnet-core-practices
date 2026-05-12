52dcf6adf77572814eaad647a18b84ffd5819157
Ceren Akçay <ceren1akcay@gmail.com>
2026-05-13 00:11:31 +0300

    İlk commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+        string [] words= input.Split(' ');
+        foreach (string word in words)
+        {
+           Console.WriteLine(
+             word.Length>1 
+             ? word.Substring(1) + word[0] 
+                : word +" "
+         );
+        }
+    }
+}

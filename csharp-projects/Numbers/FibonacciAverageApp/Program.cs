2832e4adf28e397da07b8efa6aa0bcccd07c0e91
cerenwert <ceren1akcay@gmail.com>
2026-05-22 03:11:17 +0300

    Update FibonacciAverageApp

--- csharp-projects/Numbers/FibonacciAverageApp/Program.cs
+++ csharp-projects/Numbers/FibonacciAverageApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Kaç adet Fibonacci sayısı üretilecek: ");
+        int count = int.Parse(Console.ReadLine()!);
+        int first= 0, second = 1;
+        int total = 0;
+        Console.Write("Fibonacci Sayıları: ");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(first + " ");
+            total += first;
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        double average = (double)total / count;
+        Console.WriteLine($"\nFibonacci Sayılarının Ortalaması: {average}");
+    }
+}
